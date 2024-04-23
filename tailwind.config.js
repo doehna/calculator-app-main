c798a4bbe1a43f33435f7941b9f4f2e0a3301c3d
doehna <dorota.wojdecka@gmail.com>
2024-04-23 13:31:29 +0200

    themes setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
+const { createThemes } = require('./node_modules/tw-colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "league-spartan": ['"League Spartan"', 'sans-serif']
+      },
+      colors: {
+        "main": "hsl(222, 26%, 31%)",
+        "toggle": "hsl(223, 31%, 20%)",
+        "screen": "hsl(224, 36%, 15%)"
+      },
+    },
+    plugins: [
+      createThemes({
+        light: { 
+           'primary': 'yellow',
+           'secondary': 'black',
+        },
+        medium: { 
+           'primary': 'red',
+           'secondary': 'white',
+        },
+        dark: { 
+           'primary': 'black',
+           'secondary': 'yellow',
+        },
+      })
+    ],
   },
-  plugins: [],
 }
-
