9ceab49690164084d53eb30ee6f0d0651dd3a7dd
doehna <dorota.wojdecka@gmail.com>
2024-04-27 19:44:59 +0200

    calculation functionality added

--- src/js/code.js
+++ src/js/code.js
@@ -1,20 +1,144 @@
+import { round } from 'mathjs'
+
 //var radioButtonValue = document.querySelectorAll('input[name="theme"]:checked').value;
 document.addEventListener('DOMContentLoaded', function () {
+    let screen = document.querySelector('.screen-js');
+    Reset(screen);
+
+    AddRadioButtonsClickListeners();
+    AddButtonsEventListener(screen);
+    AddResetButtonEventListener(screen);
+    AddDeleteButtonEventListener(screen);
+    AddActionButtonsEventListeners(screen);
+    AddEqualButtonEventListener(screen);
+})
+
+function AddEqualButtonEventListener(screen) {
+    let equalButton = document.querySelector('.equal-button-js');
+
+    equalButton.addEventListener('click', function () {
+        CalculateResult(screen);
+        Reset(screen);
+    })
+}
+
+function AddActionButtonsEventListeners(screen) {
+    let buttons = document.querySelectorAll('.action-button-js');
+    for (const button of buttons) {
+        button.addEventListener('click', function (e) {
+            ActionButtonEventListener(e, screen);
+        })
+    }
+}
+
+function ActionButtonEventListener(e, screen) {
+    CalculateResult(screen);
+    localStorage.setItem('action', e.target.textContent);
+}
+
+function CalculateResult(screen) {
+    // in case action button was clicked without changing the value
+    let hasChanged = localStorage.getItem('isSubmitted') === 'false';
+    
+    let result = parseFloat(localStorage.getItem('result'));
+    let action = localStorage.getItem('action');
+    let value = parseFloat(screen.innerHTML);
+
+    value = isNaN(value) ? 0 : value;
+
+    if (!isNaN(result) && hasChanged) {
+        switch (action) {
+            case '+':
+                result = result + value;
+                break;
+            case '-':
+                result = result - value;
+                break;
+            case 'x':
+                result = result * value;
+                break;
+            case '/':
+                result = result / value;
+                break;
+            default:
+                break;
+        }
+    }
+    else {
+        result = value;
+    }
+
+    localStorage.setItem('isSubmitted', 'true');
+    localStorage.setItem('result', result);
+    screen.innerHTML = round(result, 12);
+}
+
+function AddButtonsEventListener(screen) {
+    let buttons = document.querySelectorAll('.button-js');
+    for (const button of buttons) {
+        button.addEventListener('click', function () {
+            AddCharacterToScreen(screen, button);
+        })
+    }
+}
+
+function AddCharacterToScreen(screen, button) {
+    let value = screen.innerHTML.trim();
+    let isSubmitted = localStorage.getItem('isSubmitted') === 'true';
+
+    if (isSubmitted) {
+        screen.innerHTML = button.textContent;
+        localStorage.setItem('isSubmitted', 'false');
+    }
+    else {
+        let isShorterThen8Char = value.length < 8;
+        let isDot = button.textContent === '.';
+        let isDotInScreen = screen.innerHTML.indexOf('.') >= 0;
+        if (isShorterThen8Char && (!isDot || !isDotInScreen)) {
+            screen.innerHTML = screen.innerHTML + button.textContent;
+        }
+    }
+}
+
+function AddResetButtonEventListener(screen) {
+    let resetButton = document.querySelector('.reset-button-js');
+
+    resetButton.addEventListener('click', function () {
+        Reset(screen);
+        screen.innerHTML = '';
+    })
+}
+
+function Reset(screen) {
+    localStorage.setItem('result', '');
+    localStorage.setItem('action', '');
+    localStorage.setItem('isSubmitted', 'true');
+}
+
+function AddDeleteButtonEventListener(screen) {
+    let resetButton = document.querySelector('.del-button-js');
+
+    resetButton.addEventListener('click', function () {
+        screen.innerHTML = screen.innerHTML.slice(0, -1);
+    })
+}
+
+function AddRadioButtonsClickListeners() {
     let theme = SetDefaultColorTheme();
     let radioButtons = document.querySelectorAll('input[name="theme"]');
 
     for (radioButton of radioButtons) {
-        radioButton.addEventListener('click', function (e) {
+        radioButton.addEventListener('click', function () {
             ChangeColorTheme(this.id);
             SetTogglePositionByTheme(this.id);
         });
         SetFirstRadioButtonValue(radioButton, theme);
     }
-})
+}
 
 function SetTogglePositionByTheme(theme) {
     let toggleHandle = document.querySelector('.toggle-handle-js');
-    switch(theme) {
+    switch (theme) {
         case 'medium':
             toggleHandle.style.transform = 'translateX(0%)';
             break;
@@ -71,4 +195,8 @@ function MatchThemeWithBrowserPreferences(theme) {
 function SaveThemeInLocalStorage(theme) {
     localStorage.setItem('theme', theme);
     return theme;
+}
+
+function IsNullOrEmpty(text) {
+    return text === null || text === '';
 }
